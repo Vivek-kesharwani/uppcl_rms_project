cce65308bd2ada3eaa288b02fd916448e8a01dca
Vivek Kesharwani <vivekkesharwani4444@gmail.com>
2026-06-24 12:13:20 +0530

    docs/rms mock data made

---
+++ rms-backend/database/seeders/BankSettlementSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BankSettlementSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
